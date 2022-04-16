06b70eb50113d29ff25f0309c5036cb27b09f836
Nikolay <forsh_nikolay@mail.ru>
2022-04-16 16:45:45 +0300

    add MongoDB templates

--- src/main/java/ru/forsh/springbootmongoDB/SpringBootMongoDbApplication.java
+++ src/main/java/ru/forsh/springbootmongoDB/SpringBootMongoDbApplication.java
@@ -1,16 +1,15 @@
 package ru.forsh.springbootmongoDB;
 
-import org.apache.naming.factory.SendMailFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -21,7 +20,9 @@ public class SpringBootMongoDbApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
+    CommandLineRunner runner(
+            StudentRepository repository,
+            MongoTemplate mongoTemplate) {
         return args -> {
             var address = new Address(
                     "Russia",
@@ -41,10 +42,32 @@ public class SpringBootMongoDbApplication {
                     LocalDateTime.now()
             );
 
-            Query query = new Query();
-            query.addCriteria(Criteria.where("email").is(email));
-            repository.insert(student);
+           // usingMongoTemplate(repository, mongoTemplate, email, student);
+            repository.findStudentByEmail(email).ifPresentOrElse(s -> {
+                System.out.println(student + " already exists");
+            } , () -> {
+                System.out.println("inserting " + student);
+                repository.insert(student);
+            });
         };
+    }
+
+    private void usingMongoTemplate(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+
+        List<Student> students = mongoTemplate.find(query, Student.class);
+
+        if (students.size() > 1) {
+            throw new IllegalStateException("found many students with email " + email);
+        }
+
+        if (students.isEmpty()) {
+            System.out.println("inserting " + student);
+            repository.insert(student);
+        } else {
+            System.out.println(student + " already exists");
+        }
     }
 
 }
--- src/main/java/ru/forsh/springbootmongoDB/StudentRepository.java
+++ src/main/java/ru/forsh/springbootmongoDB/StudentRepository.java
@@ -2,7 +2,10 @@ package ru.forsh.springbootmongoDB;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository
         extends MongoRepository<Student, String> {
 
+    Optional<Student> findStudentByEmail(String email);
 }
