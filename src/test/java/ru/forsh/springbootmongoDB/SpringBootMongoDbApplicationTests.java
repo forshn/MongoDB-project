44e077383e431398f172d7e75064c80b9cb6b32c
Nikolay <forsh_nikolay@mail.ru>
2022-04-16 10:32:19 +0300

    Init project

---
+++ src/test/java/ru/forsh/springbootmongoDB/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.forsh.springbootmongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
